cfd07e31ddddacea44a7b1dbced036d03f1252c2
Mnkras <mnkras@gmail.com>
2011-12-26 19:40:38 -0500

    added some t()'s

--- models/event_tester.php
+++ models/event_tester.php
@@ -21,18 +21,18 @@ class EventTester extends Object{
 		$arg_list = func_get_args();
 		$event_name = array_pop($arg_list);
 		$l = new Log('event_tester', true);
-		$l->write('===Event Tester===');
+		$l->write(t('===Event Tester==='));
 		$l->write('');
-		$l->write('Fired Event: '.$event_name);
+		$l->write(t('Fired Event: ').$event_name);
 		$l->write('');
 		$numargs = func_num_args();
 		$i = 0;
 		foreach($arg_list as $thing){
 			$count = $i+1;
 			if(is_string($arg_list[$i])|| is_bool($arg_list[$i])) {
-				$l->write("Argument $count: " . $arg_list[$i]);
+				$l->write(t("Argument $count: ") . $arg_list[$i]);
 			} else {
-				$l->write("Argument $count: " . print_r($arg_list[$i], true));
+				$l->write(t("Argument $count: ") . print_r($arg_list[$i], true));
 			}
 			$i++;
 		}
